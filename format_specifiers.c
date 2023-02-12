011262b84c2f3770e5eb7dde842773a43f71f654
Onkar Mudegol <omudegol@gmail.com>
2023-02-12 16:05:42 +0530

    Add files via upload

---
+++ format_specifiers.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int age=24;
+    printf("Your age is: %d",age);
+    return 0;
+
+}
+// {
+//     int roll_no=50; 
+//     int reg_no=12217868; 
+//     int phone_number =902345678; 
+//     printf("Your roll no is %d and your registration no is %d and your phone number is %d",reg_no,roll_no,phone_number);
+//     return 0;
+// }
+
+// {
+//     int a=20;
+//     float pi=3.14;
+//     char c='O';
+//     double d=3.86767;
+//     printf("%i \n",a);
+//     printf("%f \n",pi);
+//     printf("%c",c);
+//     printf("\n %lf",d);
+// }

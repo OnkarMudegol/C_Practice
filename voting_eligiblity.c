d7a5fa93a083a15c3948aea46b5d48c40a279a2d
Onkar Mudegol <omudegol@gmail.com>
2023-02-12 16:02:16 +0530

    Add files via upload

---
+++ voting_eligiblity.c
@@ -0,0 +1,9 @@
+// write a program to chech wheteher a person is eligible to vote or not 
+// #include<stdio.h>
+// void main()
+// {
+//     int age;
+//     printf("Enter your age: ");
+//     scanf("%d",&age);
+//     (age>=18)?printf("You are eligible to vote"):printf("You are not eligible to vote");
+// }

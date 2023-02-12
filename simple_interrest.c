583d22580deedae6b16a1d919ffc25a34ce679e2
Onkar Mudegol <omudegol@gmail.com>
2023-02-12 16:02:54 +0530

    Add files via upload

---
+++ simple_interrest.c
@@ -0,0 +1,17 @@
+// write a program to find simple interest
+#include<stdio.h>
+int main()
+// {
+//     int p;
+//     int i; 
+//     int t;
+//     printf("Enter the principle amount: ");
+//     scanf("%i",&p);
+//     printf("\nEnter the interest: ");
+//     scanf("%i",&i);
+//     printf("\nEnter the time period in years: ");
+//     scanf("%i",&t);
+//     printf("\nThe simple interest is:%i",((p*i*t)/100));
+//     return 0;
+
+// }

18acea38c9d330f9a52ae2252384c1f5831fc4d9
Onkar Mudegol <omudegol@gmail.com>
2023-02-12 16:08:55 +0530

    Add files via upload

---
+++ arithematic_operators.c
@@ -0,0 +1,24 @@
+// Arithematic Opertors
+#include<stdio.h>
+int main()
+// {
+//     int a=10, b=10;
+//     printf("%d+%d=%d",a,b,(a+b));
+//     printf("\n%d-%d=%d",a,b,(a-b));
+//     printf("\n%d*%d=%d",a,b,(a*b));
+//     printf("\n%d/%d=%d",a,b,(a/b));
+//     printf("\n%d%%%d=%d",a,b,(a%b));
+// }
+// {
+//     int a;
+//     printf("Enter the value for a:");
+//     scanf("%d",&a);
+//     int b;
+//     printf("Enter the value for b:");
+//     scanf("%d",&b);
+//     printf("The value of a+b is: %d",a+b);
+//     printf("\nThe value of a-b is: %d",a-b);
+//     printf("\nThe value of a*b is: %d",a*b);
+//     printf("\nThe value of a/b is: %d",a/b);
+//     printf("\nThe value of a%b is: %d",a%b);
+// }

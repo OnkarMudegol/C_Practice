5a9db1fb44cee19cab945e5882d4ee45e4d7d50d
Onkar Mudegol <omudegol@gmail.com>
2023-02-12 16:08:03 +0530

    Add files via upload

---
+++ chocoate_distribution.c
@@ -0,0 +1,11 @@
+// write a program to distribute a given number of chocolates in a given number oof children with each child receiving and total chocolates left
+// #include<stdio.h>
+// void main()
+// {
+//     int total_choc, total_child;
+//     printf("Enter the total number of chocalates: ");
+//     scanf("%i",&total_choc);
+//     printf("Enter the number of children to be distributed in : ");
+//     scanf("%i",&total_child);
+//     printf("After equal distribution each child will have %i and %i chocalates will be remaining.",(total_choc/total_child),(total_choc%total_child));
+// }

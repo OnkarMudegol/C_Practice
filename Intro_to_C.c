b427469b55e113ed5fc5a91180b270569547ef50
Onkar Mudegol <omudegol@gmail.com>
2023-02-12 16:03:27 +0530

    Add files via upload

---
+++ Intro_to_C.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    printf("     ****      \n");
+    printf("Roll no: 50\n");
+    printf("Reg No: 12217868\n");
+    printf("Name: Onkar Mudegol\n");
+    printf("University: Lovely Professional University\n");
+    printf("Section: KOCCK\n");
+    return 0;
+}

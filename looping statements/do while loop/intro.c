0c97411348aa9326129656fa275198f43e6ecafa
Onkar Mudegol <omudegol@gmail.com>
2023-02-23 11:50:03 +0530

    Add files via upload

---
+++ looping statements/do while loop/intro.c
@@ -0,0 +1,13 @@
+// WAP to demostrate the concept of do while
+#include<stdio.h>
+int main()
+{
+    int i = 1;
+    do 
+    {
+        printf("Hello!!\n");
+        i++;
+    }
+    while(i<5);
+    return 0;
+}

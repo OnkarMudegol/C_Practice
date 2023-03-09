da1279b30e19590eeddd623ec56414ea5e5ca3bf
Onkar Mudegol <omudegol@gmail.com>
2023-03-09 14:19:06 +0530

    Function Types

---
+++ Function Types/WRT_WP.c
@@ -0,0 +1,14 @@
+// With return type and with parameter
+#include<stdio.h>
+int multiply(int a ,int b)
+{
+    return(a*b);
+}
+int main()
+{
+int multiply(int a, int b); //function prototype
+{
+    printf("7 times 8 is: %d",multiply(7,8));
+    return 0;
+}
+}
